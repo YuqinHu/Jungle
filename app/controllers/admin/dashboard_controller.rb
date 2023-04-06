1275841def1912f6d145ebb9666d97fb93183746
YuqinHu <h878688736@gmail.com>
2023-04-06 13:42:05 -0700

    admin dashboard - count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
-    def show
+  def show
+    @total_count = Product.all.count
+    @total_category_count = Category.all.count
   end
 end
