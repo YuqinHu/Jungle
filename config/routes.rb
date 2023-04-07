23f7f50316c86a523ad14e6305b80841dfa45caa
YuqinHu <h878688736@gmail.com>
2023-04-06 19:08:55 -0700

    user login,sign up,logout

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,13 @@ Rails.application.routes.draw do
     resources :categories
   end
 
+  resources :users, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   get '/about' , to: 'about#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
